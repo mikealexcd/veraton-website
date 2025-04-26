f89d1860ba7f205afc030a3dfdd3a2a5b87eba3c
mikealexcd <mikealexcd@gmail.com>
2025-04-26 12:00:20 -0400

    add pagespeed tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Teachers } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${teachers.variable} antialiased`}
       >
+        <SpeedInsights />
         {children}
       </body>
     </html>
